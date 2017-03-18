3d249e1226e4073900381520aae7690a11a2aba1
Vladyslav Stelmakhovskyi <vladyslav.stelmakhovski@volvocars.com>
2017-03-18 10:49:54 +0100

    Build fix

--- qplacesearchsuggestionreplyimpl.h
+++ qplacesearchsuggestionreplyimpl.h
@@ -34,19 +34,33 @@
 **
 ****************************************************************************/
 
-#include "qgeoerror_messages.h"
+#ifndef QPLACESEARCHSUGGESTIONREPLYIMPL_H
+#define QPLACESEARCHSUGGESTIONREPLYIMPL_H
+
+#include <QtNetwork/QNetworkReply>
+#include <QtLocation/QPlaceSearchSuggestionReply>
 
 QT_BEGIN_NAMESPACE
 
-const char NOKIA_PLUGIN_CONTEXT_NAME[] = "QtLocationQML";
-const char MISSED_CREDENTIALS[] = QT_TRANSLATE_NOOP("QtLocationQML", "Qt Location requires app_id and token parameters.\nPlease register at https://developer.here.com/ to get your personal application credentials.");
-const char SAVING_PLACE_NOT_SUPPORTED[] = QT_TRANSLATE_NOOP("QtLocationQML", "Saving places is not supported.");
-const char REMOVING_PLACE_NOT_SUPPORTED[] = QT_TRANSLATE_NOOP("QtLocationQML", "Removing places is not supported.");
-const char SAVING_CATEGORY_NOT_SUPPORTED[] = QT_TRANSLATE_NOOP("QtLocationQML", "Saving categories is not supported.");
-const char REMOVING_CATEGORY_NOT_SUPPORTED[] = QT_TRANSLATE_NOOP("QtLocationQML", "Removing categories is not supported.");
-const char PARSE_ERROR[] = QT_TRANSLATE_NOOP("QtLocationQML", "Error parsing response.");
-const char NETWORK_ERROR[] = QT_TRANSLATE_NOOP("QtLocationQML", "Network error.");
-const char CANCEL_ERROR[] = QT_TRANSLATE_NOOP("QtLocationQML", "Request was canceled.");
-const char RESPONSE_NOT_RECOGNIZABLE[] = QT_TRANSLATE_NOOP("QtLocationQML", "The response from the service was not in a recognizable format.");
+class QPlaceSearchSuggestionReplyImpl : public QPlaceSearchSuggestionReply
+{
+    Q_OBJECT
+
+public:
+    explicit QPlaceSearchSuggestionReplyImpl(QNetworkReply *reply, QObject *parent = 0);
+    ~QPlaceSearchSuggestionReplyImpl();
+
+public slots:
+    void abort();
+
+private slots:
+    void setError(QPlaceReply::Error error_, const QString &errorString);
+    void replyFinished();
+private:
+
+    QNetworkReply *m_reply;
+};
 
 QT_END_NAMESPACE
+
+#endif // QPLACESEARCHSUGGESTIONREPLYIMPL_H
